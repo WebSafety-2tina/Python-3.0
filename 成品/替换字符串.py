f0d3829e5467a76dd0b4b25f62e44e04604183fe
Marco <59686577+Marco-zcl@users.noreply.github.com>
2023-10-29 21:19:09 +0800

    10.29

---
+++ 成品/替换字符串.py
@@ -0,0 +1,2 @@
+i=input("请输入一个字符串:").replace('.','[.]')
+print(i)
---
+++ 面向过程/for 变量作用域.py
@@ -0,0 +1,8 @@
+#坚持100 每天10
+for i in range(1,101):
+    print(f"今天是第{i}天")
+#内循环
+    for j in range(1,11):
+        print(f"给他的第{j}个")
+    print("1")
+print(f"今天是第{i}天")
